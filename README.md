aef00515cbccef7c7b04592b934b5dbb3829dc25
Logan <loganm6704@gmail.com>
2025-11-25 21:46:52 -0500

    first attempt
    My first attempt used the two simplest methods in my eyes, simply multiplying the ASCII values of each letter in the chosen string and moving to the next bucket if the chosen one is not empty until an empty one is found.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # homework_5
+
+Attempt 1: The method of sorting was effective in reducing the amount of empty buckets, with 0 remaining for both table, but it definitely could have had less collisions, with the title table having 3583 collisions and the quote table having 9999 collisions. The time is took to complete the tables was 14.74 seconds.
+
---
+++ first_attempt.py
@@ -0,0 +1,81 @@
+# author: Logan Madden
+# date: 11/25/2025
+# testing different hash functions
+# approach 1: multiplying the ASCII values of the letters within the chosen title/quote and SImply checking the next bucket
+# if initial bucket is full until an open bucket is found
+
+
+import time
+
+class DataItem:
+    def __init__(self, line):
+        self.movie_name = line[0]
+        self.genre = line[1]
+        self.release_date = line[2]
+        self.director = line[3]
+        self.revenue = line[4]
+        self.rating = line[5]
+        self.min_duration = line[6]
+        self.production_company = line[7]
+        self.quote = line[8]
+        pass
+#simply multiplying the ACII calues of the letters within the chosen string
+def hashFunction(stringData):
+     key = 0
+     for i in range(0, len(stringData)):
+          if key != 0:
+               key = ord(stringData[i]) * key
+          else:
+               key = ord(stringData[i])
+     return key
+#Using the basic function of moving ot the next open slot if the initial bucket is full.
+def insertKey(hashTable, movie, key, size):
+     collisions = 0
+     while hashTable[key] != None:
+          if key >= size - 1:
+               key = 0
+          else: 
+               key += 1
+               collisions += 1
+     hashTable[key] = movie
+     return hashTable, collisions
+# finding the number of slots which still have None within them
+def emptySlots(hashTable):
+     empty = 0
+     for i in range(len(hashTable)):
+          if hashTable[i] == None:
+               empty += 1
+     return empty
+import csv
+
+# create empty hash tables
+size = 10000 
+hashTitleTable = [None] * size
+hashQuoteTable = [None] * size
+
+file = "MOCK_DATA.csv"
+counter = 0
+start = time.time()
+with open(file, 'r', newline='',  encoding="utf8") as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if counter == 0:
+               counter += 1
+               continue
+            movie = DataItem(row)
+            titleKey = hashFunction(movie.movie_name)
+            quoteKey = hashFunction(movie.quote)
+            hashTitleTable, titleCollisions = insertKey(hashTitleTable, movie, titleKey % size, size)
+            hashQuoteTable, quoteCollisions = insertKey(hashQuoteTable, movie, quoteKey % size, size)
+            counter += 1
+            if counter == 10001: # For some reaosn it would just repeat the last line, this fixed it
+                 break
+emptyTitle = emptySlots(hashTitleTable)
+emptyQuote = emptySlots(hashQuoteTable)
+print(f"the title hash table had {emptyTitle} empty slots")
+print(f"the quote hash table had {emptyQuote} empty slots")
+print(f"the title hash table had {titleCollisions} collisions")
+print(f"the quote hash table had {quoteCollisions} collisions")
+end = time.time()
+print("attempy 1: multiplying ASCII values and checking next bucket until empty one is found")
+print(f"{end-start:0.2f} seconds")
